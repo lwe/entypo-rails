9c7812ac4adae3c2c4bc4e27e7c9e2e9cf6f2089
lukas <lukas.westermann@gmail.com>
2012-11-24 11:17:18 +0100

    added some doc and correct version

--- lib/entypo-rails.rb
+++ lib/entypo-rails.rb
@@ -1,5 +1,10 @@
 require "entypo-rails/version"
 
+# The entypo-rails plugin provides access to the Entypo pictograms
+# by Daniel Bruce http://www.entypo.com in the Rails asset pipeline.
+#
+# It basically ensures that vendor/assets/fonts is on the asset
+# pipeline path.
 module Entypo
   class Engine << ::Rails::Engine
     config.assets.paths << File.expand_path "../../vendor/assets/fonts", __FILE__
--- lib/entypo-rails/version.rb
+++ lib/entypo-rails/version.rb
@@ -1,3 +1,8 @@
 module Entypo
+
+  # Gem version
   VERSION = "0.0.1"
+
+  # Entypo font version
+  FONT_VERSION = "2.0-20121031"
 end
