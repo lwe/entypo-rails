53581cec6d3a6a364b48b846f8c3c1e55952a587
lukas <lukas.westermann@gmail.com>
2012-11-24 11:39:01 +0100

    move to lib/assets otherwise not picked up by Rails

--- lib/entypo-rails.rb
+++ lib/entypo-rails.rb
@@ -1,12 +1,9 @@
-require "entypo-rails/version"
-
 # The entypo-rails plugin provides access to the Entypo pictograms
 # by Daniel Bruce http://www.entypo.com in the Rails asset pipeline.
-#
-# It basically ensures that vendor/assets/fonts is on the asset
-# pipeline path.
 module Entypo
+
+  # Dummy engine so that lib/assets/stylesheets and vendor/assets/fonts
+  # are picked up by Rails asset pipeline.
   class Engine < ::Rails::Engine
-    config.assets.paths << File.expand_path("../../vendor/assets/fonts", __FILE__)
   end
 end
