e4117ef9943e2d0c7d77a923f84a7f3d64e3e60b
lukas <lukas.westermann@gmail.com>
2013-12-19 12:03:28 +0100

    stil use 1.8 hash-rocket syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 # Enable charmap in development mode, also makes it simpler to test it
 Rails.application.routes.draw do
-  get 'entypo/charmap', to: 'entypo/charmap#index' if Entypo.charmap?
+  get 'entypo/charmap', :to => 'entypo/charmap#index' if Entypo.charmap?
 end
